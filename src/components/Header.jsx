b6bf2539924607ddd3acd36efb83b83e2f93ae73
Emmanuel <ememeemmanuel3@gmail.com>
2023-01-18 15:41:54 +0000

    Final Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import HighlightIcon from "@material-ui/icons/Highlight";
 
-const Header = () => {
+function Header() {
   return (
     <header>
-      <h1>Keeper App</h1>
+      <h1>
+        <HighlightIcon />
+        Keeper App
+      </h1>
     </header>
   );
-};
+}
 
 export default Header;
